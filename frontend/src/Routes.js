7e056c608b50d90847570ed48c7eb1402d73b9d2
Yslamguly <islamguly28@gmail.com>
2023-03-26 06:58:00 +0200

    feat<frontend>: added register route

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {Home} from "./pages/Home";
 import {NavBar} from "./components/NavBar";
 import {Footer} from "./components/Footer";
+import Register from "./pages/Register";
 
 export const RouterController = () =>{
     return(
@@ -9,6 +10,8 @@ export const RouterController = () =>{
             <NavBar/>
             <Routes>
                 <Route path={'/home'} element={<Home/>}/>
+                <Route path={'/register'} element={<Register/>}/>
+
             </Routes>
             <Footer/>
         </Router>
