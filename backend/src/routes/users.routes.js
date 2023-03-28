87ddcdc9c1f2d20f8d51f8bfb5efe67ed9fcf304
Hanna Hasson <hsoonhanna0@gmail.com>
2023-03-28 13:34:36 +0200

    <feat>login-system

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const usersController = require('../controllers/users.controllers')
 
 router.post('/register',usersController.register);
+router.get('/login',usersController.login);
+
 
 
 module.exports = router;
