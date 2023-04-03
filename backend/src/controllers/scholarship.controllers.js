be23fad89af0df555f0066eee0007ed3cdd2a27c
MaximusDD2002 <2maxim2@mail.ru>
2023-04-03 21:12:14 +0300

    Made a function
    Made a function for fetching scholarships from the database by category ID

--- backend/src/controllers/scholarship.controllers.js
+++ backend/src/controllers/scholarship.controllers.js
@@ -34,3 +34,24 @@ exports.getScholarshipsById = (req,res) => {
           });
       
 }
+
+exports.getScholarshipByCategoryId = (req, res) => {
+
+    const { category_id } = req.params;
+
+    db.select('*')
+        .from('scholarship_finder.scholarship_category')
+        .where({ category_id })
+        .then((data) => {
+            // Return the scholarship data retrieved from the database
+            if (data.length === 0) {
+                return res.status(404).json({ message: 'Scholarship not found' });
+            }
+            res.status(200).json(data[0]);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).json({ message: 'Internal server error' });
+        });
+
+}
