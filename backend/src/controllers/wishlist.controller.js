3d67c2d863ce4aa33e2708c56fca404a7eb9bf69
yslamgulypirgulyyev <islamguly28@gmail.com>
2023-05-11 16:59:46 +0200

    feat<backend>:added function to delete a scholarship from user's wish list

--- backend/src/controllers/wishlist.controller.js
+++ backend/src/controllers/wishlist.controller.js
@@ -3,6 +3,10 @@ const db = require('../../config/db');
 exports.getUserWishList = (req,res)=>{
     const {user_id} = req.params
 
+    if (isNaN(parseInt(user_id))) {
+        res.status(400).json({ message: 'Invalid user_id' });
+    }
+
     db.select('scholarship_finder.scholarships.id',
         'scholarship_finder.scholarships.title',
         'scholarship_finder.scholarships.description',
@@ -15,7 +19,7 @@ exports.getUserWishList = (req,res)=>{
             this.on(`scholarship_finder.wish_list_item.scholarship_id`, '=', `scholarship_finder.scholarships.id`)
         })
         .join('scholarship_finder.wish_list', function () {
-            this.on(`scholarship_finder.wish_list_item.wish_list_id`, '=', `scholarship_finder.scholarships.id`)
+            this.on(`scholarship_finder.wish_list_item.wish_list_id`, '=', `scholarship_finder.wish_list.id`)
         })
         .join('scholarship_finder.scholarship_category', function (){
             this.on('scholarship_finder.scholarships.id','=','scholarship_finder.scholarship_category.scholarship_id')
@@ -25,7 +29,34 @@ exports.getUserWishList = (req,res)=>{
         })
         .where(`scholarship_finder.wish_list.user_id`, '=', user_id)
         .then(data => {
+            console.log(data)
             data.length ? res.json(data) : res.status(200).json({message: 'You have no scholarship in your wishlist'})
         })
         .catch(err => res.status(500).json(err))
+}
+
+exports.deleteScholarshipFromWishList = async (req,res)=>{
+    const {wish_list_item_scholarship_id} = req.body;
+
+    const {user_id} = req.params;
+
+    if (wish_list_item_scholarship_id === null) {
+        res.sendStatus(400)
+    }
+    if(isNaN(parseInt(wish_list_item_scholarship_id))){
+        res.status(400).json({ message: 'Invalid scholarship_id' });
+    }
+    if (isNaN(parseInt(user_id))) {
+        res.status(400).json({ message: 'Invalid user_id' });
+    }
+
+    const wish_list_id = await db.select('id').from('scholarship_finder.wish_list').where('user_id', '=', user_id)
+
+
+    db('scholarship_finder.wish_list_item')
+        .where('wish_list_id', '=', wish_list_id[0].id)
+        .andWhere('scholarship_id', '=', wish_list_item_scholarship_id)
+        .del()
+        .then(() => res.status(200).json({message: 'Scholarship has been deleted successfully'}))
+        .catch(err => res.status(500).json({message:err}))
 }
--- backend/src/routes/wishlist.routes.js
+++ backend/src/routes/wishlist.routes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const wishListControllers = require('../controllers/wishlist.controller')
 
 router.get('/getWishList/:user_id',wishListControllers.getUserWishList)
-
+router.delete('/deleteScholarshipFromWishList/:user_id',wishListControllers.deleteScholarshipFromWishList)
 module.exports = router;
