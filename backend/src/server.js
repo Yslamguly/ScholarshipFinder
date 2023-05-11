1096033988cf42c04b43cd392df7aa95dec5e654
yslamgulypirgulyyev <islamguly28@gmail.com>
2023-05-11 17:01:16 +0200

    feat<backend>:Access-Control-Allow-Methods: allow a delete method

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ const port = 8080
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
