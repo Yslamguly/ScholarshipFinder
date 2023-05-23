4ccde5b47619bd79a2acc93fbb80035d6b730d78
yslamgulypirgulyyev <islamguly28@gmail.com>
2023-05-23 10:56:37 +0200

    feat<frontend>:implemented search bar

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import Landing from '../components/Landing';
 import CardList from "../components/Cards/CardList";
 
-export const Home = () =>{
+export const Home = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearch = (query) => {
+        setSearchQuery(query);
+    };
     return (
         <>
-        <Landing />
-        <div>
-        <h1 style={{ textAlign: 'center',marginTop:'30px', marginBottom:'30px' }}>The Latest Scholarships</h1>
-        </div>
-        <CardList link={'http://localhost:8080/scholarship'} deleteButton={false}/>
+            <Landing onSearch={handleSearch}/>
+            <div>
+                <h1 style={{textAlign: 'center', marginTop: '30px', marginBottom: '30px'}}>The Latest Scholarships</h1>
+            </div>
+            <CardList link={`http://localhost:8080/scholarship?search=${searchQuery}`} deleteButton={false}/>
         </>
-      );
+    );
 }
